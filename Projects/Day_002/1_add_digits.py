4606dbc3381aab51d87fa03df11dfe366cb5366d
mohliyet <mohaliyeu@gmail.com>
2023-11-19 14:31:10 +0300

    day two: two more simple projects added - two digits adder and bmi calculator

---
+++ Projects/Day_002/1_add_digits.py
@@ -0,0 +1,7 @@
+two_digit_number = input('Write two digit number: ')
+
+digit_one = int(str(two_digit_number[0]))
+digit_two = int(str(two_digit_number[1]))
+
+sum_of_the_digits = digit_one+digit_two
+print(sum_of_the_digits)
---
+++ Projects/Day_002/2_bmi_calculator.py
@@ -0,0 +1,6 @@
+height = float(input('write your height: '))
+weight = float(input('write your weight: '))
+
+bmi = round(weight/height**2)
+
+print(bmi)
