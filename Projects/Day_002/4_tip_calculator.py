6f43ca9f2780534fe95eebbc5875dea5120c5d57
M. A. <mohaliyeu@gmail.com>
2023-11-19 19:01:45 +0300

    just to test after access key is chenged

--- Projects/Day_002/4_tip_calculator.py
+++ Projects/Day_002/4_tip_calculator.py
@@ -1,7 +1,7 @@
 #If the bill was $150.00, split between 5 people, with 12% tip. 
 
 #Each person should pay (150.00 / 5) * 1.12 = 33.6
-#Format the result to 2 decimal places = 33.60
+#Format the result to 2 decimal places = 33.6
 
 print('Welcome to the tip calculator.')
 total_bill = float(input('What was the total bill? $'))
