f0d5b03fdecdf846c9a6dafecb9c381bd3990102
M. A. <mohaliyeu@gmail.com>
2023-11-20 21:34:00 +0300

    bill variable is intitialized in the pizza order python script

--- Projects/Day_003/4_pizza_order.py
+++ Projects/Day_003/4_pizza_order.py
@@ -2,6 +2,8 @@ print("Thank you for choosing Python Pizza Deliveries!")
 size = input() # What size pizza do you want? S, M, or L
 add_pepperoni = input() # Do you want pepperoni? Y or N
 extra_cheese = input() # Do you want extra cheese? Y or N
+
+bill = 0
 if size=='S':
   bill = 15
 if size=='M':
