7c928598caa7de0101fc72a67a6baf25ecc4e07d
M. A. <mohaliyeu@gmail.com>
2023-11-25 17:30:35 +0300

    prime number checkeer function between 1 and 100 - updated

--- Projects/Day_008/3_prime_number_checker.py
+++ Projects/Day_008/3_prime_number_checker.py
@@ -1,11 +1,22 @@
+# def prime_checker(number):
+#   cnt = 0
+#   for i in range(1,number+1):
+    # if number%i == 0:
+    #   cnt+=1
+#   if cnt==2:
+    # print("It's a prime number.")
+#   else:
+    # print("It's not a prime number.")
+
 def prime_checker(number):
-  cnt = 0
-  for i in range(1,number+1):
-    if number%i == 0:
-      cnt+=1
-  if cnt==2:
-    print("It's a prime number.")
-  else:
-    print("It's not a prime number.")
-n = int(input('Write a number: ')) # Check this number
+    is_prime = True
+    for i in range(2,number):
+        if number%i==0:
+            is_prime = False
+    if is_prime:
+        print("It's a prime number.")
+    else:
+        print("It's not a prime number.")
+
+n = int(input('Write a number: '))
 prime_checker(number=n)
